d3bfe8c3f73a6e3a2a4ad014b8daafaf2e20f306
ayoubcode-source <ayoubelhadielhadi@gmail.com>
2024-10-04 15:12:36 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// // Wait for the document to be fully loaded
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Select the navbar
+//     let navbar = document.querySelector('.navbar');
+//     let a = document.querySelector('#nav-elements a')
+//     console.log(a)
+
+//     // Add a scroll event listener to the window
+//     window.addEventListener('scroll', function() {
+//         // If the user scrolls more than 50px, add the 'scrolled' class to the navbar
+//         if (window.scrollY > 120) {
+//             navbar.classList.add('scrolled');
+
+//         } else {
+//             navbar.classList.remove('scrolled');
+
+//         }
+//     });
+// });
